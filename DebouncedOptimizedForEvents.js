10b3d2cc15e7f146d9b4344fa7e222623619a401
Fabio Vergani <fabio.vergani.1975@gmail.com>
2019-01-02 15:23:16 +0100

    Update DebouncedOptimizedForEvents.js

--- DebouncedOptimizedForEvents.js
+++ DebouncedOptimizedForEvents.js
@@ -1,6 +1,4 @@
 (w=>{
-	const Tset=w.setTimeout,
-  Tclear=w.clearTimeout,
-  debounced=(f,n=300)=>{let e=null;return x=>{if(null===e){e=Tset(()=>{f(x);Tclear(e);e=null},n)}}};
+ const Tset=w.setTimeout,Tclear=w.clearTimeout,debounced=(f,n=300)=>{let e=null;return x=>{if(null===e){e=Tset(()=>{f(x);Tclear(e);e=null},n)}}};
 
 })(window);
